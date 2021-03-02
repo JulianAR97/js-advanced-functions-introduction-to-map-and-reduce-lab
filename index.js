b4f21e40c1ccd02ac7b3f0016360bf9e845e1e4d
Julian <julianrosenthal@yahoo.com>
2021-03-02 15:36:06 -0700

    commit tree a493c2fc13381c73772cdfce1dd8bce6df42f956

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your code here
+
+// Map Functions
+let mapToNegativize = arr => arr.map(ele => ele * -1)
+let mapToNoChange = arr => arr
+let mapToDouble = arr => arr.map(ele => ele * 2)
+let mapToSquare = arr => arr.map(ele => ele * ele)
+
+// Reduce Functions
+let reduceToTotal = (arr, start = 0) => arr.reduce((a, b) => a + b, start)
+let reduceToAllTrue = arr => arr.reduce((a, b) => !!a && !!b)
+let reduceToAnyTrue = arr => arr.reduce((a, b) => !!a || !!b)
